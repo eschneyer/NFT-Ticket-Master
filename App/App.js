5e062b3899f88770685ac9b3cea6c958b422787e
Ella Schneyer <79551618+eschneyer@users.noreply.github.com>
2022-07-14 23:02:59 -0400

    Create App.js

---
+++ App/App.js
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import Navigator from './homeStack';
+import Home from './home';
+import Header from './components/header';
+
+export default function App() {
+  return (
+    <Navigator />
+  );
+}
